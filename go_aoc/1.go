cf361e1fb951eb4191f5a64917b29f5b3937c742
Drew Nase <dnase@drews-mbp.lan>
2018-12-01 01:59:01 -0500

    wrote solution in golang, cleaned up python code

---
+++ go_aoc/1.go
@@ -0,0 +1,70 @@
+// Make sure you have a file called input_1 with a number on each line in the
+// same directory from which you are executing.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func readLines(path string) ([]int, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []int
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		buff, e := strconv.Atoi(scanner.Text())
+		if e != nil {
+			// ???
+		} else {
+			lines = append(lines, buff)
+		}
+	}
+	return lines, scanner.Err()
+}
+
+func sum(r []int) int {
+	retval := 0
+	for _, i := range r {
+		retval += i
+	}
+	return retval
+}
+
+func contains(s []int, e int) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	current_freq := 0
+	previous_freqs := []int{0}
+	freqs, e := readLines("input_1")
+	if e != nil {
+		fmt.Printf("failed to read file")
+		os.Exit(3)
+	}
+	// Question 1
+	fmt.Printf("Question 1: %d\n", sum(freqs))
+	// Question 2
+	for true {
+		for _, freq := range freqs {
+			current_freq += freq
+			if contains(previous_freqs, current_freq) {
+				fmt.Printf("Question 2: %d\n", current_freq)
+				os.Exit(0)
+			}
+			previous_freqs = append(previous_freqs, current_freq)
+		}
+	}
+}
--- py_aoc/1.py
+++ py_aoc/1.py
@@ -1,11 +1,10 @@
 # save input list as "input_1" - one number per line
-sequence = open("input_1").readlines()
+seq = [int(s) for s in open("input_1").readlines()]
 
-seq = [int(s) for s in sequence]
 # question 1
 print("Question 1: %d" % sum(seq))
 
-# question 2
+# question 2 - naive solution, but it works
 previous_freqs = [0]
 current_freq = 0
 while 1:
