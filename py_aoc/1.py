262373dff90254024a076f43e9473089cef50e60
Drew Nase <dnase@drews-mbp.lan>
2018-12-01 00:52:51 -0500

    day 1

---
+++ py_aoc/1.py
@@ -0,0 +1,17 @@
+# save input list as "input_1" - one number per line
+sequence = open("input_1").readlines()
+
+seq = [int(s) for s in sequence]
+# question 1
+print("Question 1: %d" % sum(seq))
+
+# question 2
+previous_freqs = [0]
+current_freq = 0
+while 1:
+    for delta in seq:
+        current_freq += delta
+        if current_freq in previous_freqs:
+            print("Question 2: %d" % current_freq)
+            quit()
+        previous_freqs.append(current_freq)
