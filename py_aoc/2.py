d97baf7f5617a1fddc0a624398cc0c8df514c702
Drew Nase <dnase@drews-mbp.lan>
2018-12-02 00:57:15 -0500

    clean up pick up put away

--- py_aoc/2.py
+++ py_aoc/2.py
@@ -15,20 +15,20 @@ def check3(instr):
             return 1
     return 0
 
-def levenshteinDistance(s1, s2):
+def levenshtein(s1, s2):
     if len(s1) > len(s2):
         s1, s2 = s2, s1
 
-    distances = range(len(s1) + 1)
+    d = range(len(s1) + 1)
     for i2, c2 in enumerate(s2):
-        distances_ = [i2+1]
+        d_ = [i2+1]
         for i1, c1 in enumerate(s1):
             if c1 == c2:
-                distances_.append(distances[i1])
+                d_.append(d[i1])
             else:
-                distances_.append(1 + min((distances[i1], distances[i1 + 1], distances_[-1])))
-        distances = distances_
-    return distances[-1]
+                d_.append(1 + min((d[i1], d[i1 + 1], d_[-1])))
+        d = d_
+    return d[-1]
 
 def intersection(s1, s2):
     if len(s1) == len(s2):
@@ -47,7 +47,7 @@ print("Q1: %d" % (count2 * count3))
 
 for s in seq:
     for subs in seq:
-        if levenshteinDistance(s, subs) == 1:
+        if levenshtein(s, subs) == 1:
             res = intersection(s, subs)
             print("Q2: %s" % res)
             quit()
