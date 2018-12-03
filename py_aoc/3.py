f196e25c7f0dea5fd6ad9878c5c2755f4373d610
Drew Nase <dnase@drews-mbp.lan>
2018-12-03 01:56:41 -0500

    cleanup;

--- py_aoc/3.py
+++ py_aoc/3.py
@@ -8,26 +8,26 @@ def parse(line):
 
 def overlap(seq):
     d = [parse(line) for line in seq]
-    overlaps = defaultdict(int)
+    pointmap = defaultdict(int)
     for _, x, y, w, h in d:
         for i in range(x, x + w):
             for j in range(y, y + h):
-                overlaps[(i, j)] += 1
+                pointmap[(i, j)] += 1
 
     buff = 0
-    for v in overlaps.values():
+    for v in pointmap.values():
         if v > 1:
             buff += 1
     print("Q1: %d" % buff)
-    return overlaps
+    return pointmap
 
-def id_no_overlap(seq, overlaps):
+def id_no_overlap(seq, pointmap):
     d = [parse(line) for line in seq]
     for id, x, y, w, h in d:
         valid = True
         for i in range(x, x + w):
             for j in range(y, y + h):
-                if overlaps[(i, j)] != 1:
+                if pointmap[(i, j)] != 1:
                     valid = False
                     break
             if not valid:
@@ -36,5 +36,5 @@ def id_no_overlap(seq, overlaps):
             print("Q2: %s" % id[1:])
 
 seq = [s.strip() for s in open("../data/input_3").readlines()]
-o = overlap(seq)
-id_no_overlap(seq, o)
+p = overlap(seq)
+id_no_overlap(seq, p)
