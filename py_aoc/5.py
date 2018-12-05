b58ee6d6df0172a6775cc61056b356b001038f1c
Drew Nase <dnase@drews-mbp.lan>
2018-12-05 01:10:53 -0500

    day 5

--- py_aoc/5.py
+++ py_aoc/5.py
@@ -1,6 +1,40 @@
+import operator
 from util import parse_file
 
 def parsefunc(s):
     return s.strip()
 
-print(parse_file("../data/input_5", parsefunc))
+def str_delete(s, idx1, idx2):
+    return s[:idx1] + s[idx2+1:]
+
+seq = parse_file("../data/input_5", parsefunc)[0]
+
+# part 1
+def reduce_sequence(seq):
+    check_len = len(seq)
+    for c in "abcdefghijklmnopqrstuvwxyz":
+		seq = seq.replace(c+c.upper(),"").replace(c.upper()+c,"")
+    seq_len = len(seq)
+    return ((seq_len != check_len), seq)
+
+# part 2
+def part2(seq):
+    lengths = {}
+    for c in "abcdefghijklmnopqrstuvwxyz":
+        buff = seq.replace(c,"").replace(c.upper(),"")
+        recur = True
+        while recur:
+            (recur, buff) = reduce_sequence(buff)
+        lengths[c] = len(buff)
+    return lengths
+
+buff1 = seq
+recur = True
+while recur:
+    (recur, buff1) = reduce_sequence(buff1)
+
+print(len(buff1))
+
+p2 = part2(seq)
+sorted_p2 = sorted(p2.items(), key=operator.itemgetter(1))
+print(sorted_p2[0][1])
