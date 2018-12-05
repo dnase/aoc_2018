02e5043462f0e354407770a38c9b92d7621e5bf6
Drew Nase <dnase@drews-mbp.lan>
2018-12-05 11:21:22 -0500

    cleaned up solution to day 5

--- py_aoc/5.py
+++ py_aoc/5.py
@@ -6,13 +6,20 @@ def parsefunc(s):
 
 seq = parse_file("../data/input_5", parsefunc)[0]
 
-# part 1
 def reduce_sequence(seq):
     check_len = len(seq)
     for c in "abcdefghijklmnopqrstuvwxyz":
 		seq = seq.replace(c+c.upper(),"").replace(c.upper()+c,"")
     seq_len = len(seq)
     return ((seq_len != check_len), seq)
+
+# part 1
+def part1(seq):
+    buff1 = seq
+    recur = True
+    while recur:
+        (recur, buff1) = reduce_sequence(buff1)
+    return len(buff1)
 
 # part 2
 def part2(seq):
@@ -23,15 +30,10 @@ def part2(seq):
         while recur:
             (recur, buff) = reduce_sequence(buff)
         lengths[c] = len(buff)
-    return lengths
+    lengths_sorted = sorted(lengths.items(), key=operator.itemgetter(1))
+    return lengths_sorted[0][1]
 
-buff1 = seq
-recur = True
-while recur:
-    (recur, buff1) = reduce_sequence(buff1)
 
-print(len(buff1))
 
-p2 = part2(seq)
-sorted_p2 = sorted(p2.items(), key=operator.itemgetter(1))
-print(sorted_p2[0][1])
+print("Q1: %d" % part1(seq))
+print("Q2: %d" % part2(seq))
