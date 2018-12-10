bdfd89fb509f059a6749c0d5bc20918952ca4336
Drew Nase <dnase@drews-mbp.lan>
2018-12-09 20:08:55 -0500

    much faster, more efficient solution to day 8

--- py_aoc/8.py
+++ py_aoc/8.py
@@ -1,38 +1,61 @@
-from sys import setrecursionlimit
-from sets import Set
 from util import parse_file
-from collections import defaultdict
 
-def total_metadata(seq, i, metadata):
+class node:
+    def __init__(self, num_children, num_meta_ent):
+        self.num_children = num_children
+        self.num_meta_ent = num_meta_ent
+        self.child_nodes = []
+        self.metadata = []
+    def add_child_node(self, child_node):
+        self.child_nodes.append(child_node)
+    def add_metadata(self, metadata):
+        self.metadata.append(metadata)
+
+def get_nodes(seq, i):
+    nodes = []
     num_children = seq[i]
-    num_meta_ent = seq[i + 1]
+    num_meta_ent = seq[i+1]
     i += 2
+    thisnode = node(num_children, num_meta_ent)
     for j in range(num_children):
-        (i, _) = total_metadata(seq, i, metadata)
-    for j in range(num_meta_ent):
-        metadata.append(int(seq[i + j]))
-    return (i + num_meta_ent, sum(metadata))
+        (i, child_nodes) = get_nodes(seq, i)
+        for cn in child_nodes:
+            thisnode.add_child_node(cn)
+    for k in range(num_meta_ent):
+        thisnode.add_metadata(seq[i + k])
+    nodes.append(thisnode)
+    return(i + num_meta_ent, nodes)
 
-def root_node_value(seq):
-    [num_children, num_meta_ent] = seq[:2]
-    seq = seq[2:]
-    buff = []
-    for i in range(num_children):
-        s, seq = root_node_value(seq)
-        buff.append(s)
-    if num_children == 0:
-        return (sum(seq[:num_meta_ent]), seq[:num_meta_ent])
+
+def total_metadata(nodes):#seq, i, metadata):
+    tally = 0
+    for n in nodes:
+        tally += sum(n.metadata)
+        tally += total_metadata(n.child_nodes)
+    return tally
+
+def node_value(node):#seq):
+    if node.num_children == 0:
+        return sum(node.metadata)
     else:
-        return (sum(buff[x - 1] for x in seq[:num_meta_ent] if x > 0 and x <= len(buff)), seq[num_meta_ent:])
+        t = 0
+        for i in node.metadata:
+            try:
+                t += node_value(node.child_nodes[i - 1])
+            except:
+                pass
+        return t
 
 def parsefunc(s):
     return s.strip()
 
 def part1(seq):
-    return total_metadata(seq, 0, [])[1]
+    n = get_nodes(seq, 0)[1]
+    return total_metadata(n)
 
 def part2(seq):
-    return root_node_value(seq)[0]
+    n = get_nodes(seq, 0)[1][0]
+    return node_value(n)
 
 seq = [int(c) for c in parse_file("../data/input_8", parsefunc)[0].split()]
 
