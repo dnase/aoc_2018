7b148d2f7e5f580de4f1881322d9863dcecf01d8
Drew Nase <dnase@drews-mbp.lan>
2018-12-10 15:20:41 -0500

    slightly more secure, whatever

--- py_aoc/util.py
+++ py_aoc/util.py
@@ -1,11 +1,11 @@
-import urllib2
 import os.path
 import requests
 # common utilities for Advent of Code
 def get_data(exnum, parsefunc):
     filepath = "../data/input_%d" % exnum
     if not os.path.isfile(filepath):
-        cookies = {'session': '53616c7465645f5f937a9f5b169013c44a6d4217565e2d9cbdb67ea279ae9dcbe2c84bd90a96e85174a35c257b6a4b94'}
+        token = open("../data/token").read().strip()
+        cookies = {'session': token}
         urlpath = "https://adventofcode.com/2018/day/%d/input" % exnum
         fh = open(filepath, 'w')
         fh.write(requests.get(urlpath, cookies=cookies).text)
