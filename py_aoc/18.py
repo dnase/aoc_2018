4a88683dbec064aed86c3473022f7406594c5d2b
Drew Nase <dnase@drews-mbp.lan>
2018-12-10 15:30:30 -0500

    added skeletal templates for each day

---
+++ py_aoc/18.py
@@ -0,0 +1,16 @@
+from util import get_data
+
+def parsefunc(s):
+    return s.strip()
+
+def part1():
+    return True
+
+
+def part2():
+    return True
+
+seq = get_data(18, parsefunc)
+
+print("P1: ", part1())
+print("P2: ", part2())
