3fc96251eaf6eab717c0a1cceb67fda9028c8d27
Drew Nase <dnase@drews-mbp.lan>
2018-12-10 01:34:41 -0500

    added visualization for day 6

---
+++ py_aoc/voronoi.py
@@ -0,0 +1,44 @@
+# original code modified from https://rosettacode.org/wiki/Voronoi_diagram#Python
+from PIL import Image
+import random
+import math
+from util import parse_file
+
+def generate_voronoi_diagram(width, height, num_cells, coords):
+    image = Image.new("RGB", (width, height))
+    putpixel = image.putpixel
+    imgx, imgy = image.size
+    nx = []
+    ny = []
+    nr = []
+    ng = []
+    nb = []
+
+    for c in coords:
+        nx.append(c[0])
+    	ny.append(c[1])
+        nr.append(random.randrange(256))
+    	ng.append(random.randrange(256))
+    	nb.append(random.randrange(256))
+    for y in range(imgy):
+    	for x in range(imgx):
+    		dmin = abs(imgx-1) + abs(imgy-1)
+    		j = -1
+    		for i in range(num_cells):
+    			d = abs(nx[i]-x) + abs(ny[i]-y)
+    			if d < dmin:
+    				dmin = d
+    				j = i
+    		putpixel((x, y), (nr[j], ng[j], nb[j]))
+    for c in coords:
+        putpixel((c[0], c[1]), (255, 255, 255))
+    image.save("VoronoiDiagram.png", "PNG")
+    image.show()
+
+def parsefunc(s):
+    buff = [n.strip() for n in s.split(",")]
+    return (int(buff[0]), int(buff[1]))
+
+seq = parse_file("../data/input_6", parsefunc)
+
+generate_voronoi_diagram(400, 400, len(seq), seq)
