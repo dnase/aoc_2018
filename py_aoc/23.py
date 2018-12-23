a5d029065db4245eda2ff936a74e4c9af1c2456a
Drew Nase <dnase@drews-mbp.lan>
2018-12-23 15:27:54 -0500

    day 23

--- py_aoc/23.py
+++ py_aoc/23.py
@@ -1,4 +1,5 @@
 from util import get_data
+from Queue import PriorityQueue
 
 def parsefunc(s):
     parts = s.split(', ')
@@ -11,16 +12,25 @@ def distance(p1, p2):
 
 def part1(seq):
     m = max(seq, key=lambda n: n[1])
-    total = 0
-    for n in seq:
-        if distance(n[0], m[0]) <= m[1]:
-            total += 1
-    return total
+    return sum([1 for n in seq if distance(n[0], m[0]) <= m[1]])
 
-def part2():
-    return True
+def part2(seq):
+    q = PriorityQueue()
+    for n in seq:
+        x,y,z,r = n[0][0], n[0][1], n[0][2], n[1]
+        d = abs(x) + abs(y) + abs(z)
+        q.put((max(0, d - r),1))
+        q.put((d + r,-1))
+    c, m, r = 0, 0, 0
+    while not q.empty():
+        dist, e = q.get()
+        c += e
+        if c > m:
+            r = dist
+            m = c
+    return r
 
 seq = get_data(23, parsefunc)
 
 print("P1: ", part1(seq))
-print("P2: ", part2())
+print("P2: ", part2(seq))
